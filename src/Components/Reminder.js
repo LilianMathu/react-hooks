e0f78b318e62463fca1b1da85d57e993506e0a7d
Lilian Mathu <lilianmathu2018@gmail.com>
2021-12-14 23:34:41 +0300

    Conditionally run effects

--- src/Components/Reminder.js
+++ src/Components/Reminder.js
@@ -2,18 +2,25 @@ import React, { useState, useEffect } from "react";
 
 function Reminder() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   const changeCount = () => {
     setCount(count + 1);
   };
 
+  const changeName = (e) => {
+    setName(e.target.value);
+  };
+
   useEffect(() => {
+    console.log(`updated ${count} times`);
     document.title = `clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
       <button onClick={changeCount}>Count: {count}</button>
+      <input type="text" value={name} onChange={changeName} />
     </div>
   );
 }
