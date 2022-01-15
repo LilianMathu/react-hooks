befceac4bb50e023446719b810440d25d33a41db
Lilian Mathu <mathulilian97@gmail.com>
2022-01-15 13:28:59 +0300

    3#: If-Else statement

--- src/react basics/ConditionalRendering.js
+++ src/react basics/ConditionalRendering.js
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 const ConditionalRendering = () => {
-  //React render conditional 1#: Inline If with logical operator
+  //1#: Inline If with logical operator
   //  const [loggedIn, setLoggedIn] = useState(false);
   //  useEffect(() => {
   //    setLoggedIn(true);
   //  });
   //  return <div>{(loggedIn && <h1>Hello Lilian</h1>) || "Hello Stranger"}</div>;
+  //#2: Inline if-else with conditional operator
+  //const [loggedIn, setLoggedIn] = useState(false);
+  //useEffect(() => {
+  //  setLoggedIn(true);
+  //});
+  //return <div>{loggedIn ? <h1>Hello Lilian</h1> : <h1>Hello Guest</h1>}</div>;
 
-  //React render conditional #2: Inline if-else with conditional operator
-
-  const [loggedIn, setLoggedIn] = useState(false);
+  //3#: If-Else statement
+  const [loggedIn, setloggedIn] = useState(false);
 
   useEffect(() => {
-    setLoggedIn(true);
+    setloggedIn(true);
   });
 
-  return <div>{loggedIn ? <h1>Hello Lilian</h1> : <h1>Hello Guest</h1>}</div>;
+  if (loggedIn) {
+    return (
+      <div>
+        <h1>Hello Lilian</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Hello Stranger</h1>
+      </div>
+    );
+  }
 };
 
 export default ConditionalRendering;
