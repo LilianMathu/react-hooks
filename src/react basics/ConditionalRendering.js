863be59166ab0677c3073bdc2e9fbc4fd23b5a51
Lilian Mathu <mathulilian97@gmail.com>
2022-01-15 13:21:55 +0300

    #2: Inline if-else with conditional operator

--- src/react basics/ConditionalRendering.js
+++ src/react basics/ConditionalRendering.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 
 const ConditionalRendering = () => {
   //React render conditional 1#: Inline If with logical operator
+  //  const [loggedIn, setLoggedIn] = useState(false);
+  //  useEffect(() => {
+  //    setLoggedIn(true);
+  //  });
+  //  return <div>{(loggedIn && <h1>Hello Lilian</h1>) || "Hello Stranger"}</div>;
+
+  //React render conditional #2: Inline if-else with conditional operator
 
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -9,7 +16,7 @@ const ConditionalRendering = () => {
     setLoggedIn(true);
   });
 
-  return <div>{(loggedIn && <h1>Hello Lilian</h1>) || "Hello Stranger"}</div>;
+  return <div>{loggedIn ? "Hello Lilian" : "Hello stranger"}</div>;
 };
 
 export default ConditionalRendering;
